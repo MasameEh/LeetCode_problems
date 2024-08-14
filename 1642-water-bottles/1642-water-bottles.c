85b0861d85064cb44ec36fdeec902f5f2535b242
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2024-08-14 17:47:29 +0300

    Time: 2 ms (59.28%), Space: 5.4 MB (63.26%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.c
@@ -0,0 +1,12 @@
+int numWaterBottles(int numBottles, int numExchange) {
+    int total = numBottles, bottleExchanged = 0;
+
+    while(numBottles >= numExchange)
+    {
+        bottleExchanged = numBottles / numExchange; // exchanged the empty bottles to get full water bottles.
+        total += bottleExchanged; // add to the total bottles we had 
+        numBottles = numBottles % numExchange; // see how many bottles left
+        numBottles += bottleExchanged; // add bottles we get to the bottles we have
+    }
+    return total;
+}
