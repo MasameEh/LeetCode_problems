d096dc71f40d767908082bdeee667af16ec78e4e
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2024-08-14 17:47:43 +0300

    Time: 329 ms (39.83%), Space: 145.4 MB (94.92%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.dart
@@ -0,0 +1,14 @@
+class Solution {
+  int numWaterBottles(int numBottles, int numExchange) {
+    int total = numBottles, bottleExchanged = 0;
+
+    while(numBottles >= numExchange)
+    {
+        bottleExchanged = numBottles ~/ numExchange; // exchanged the empty bottles to get full water bottles.
+        total += bottleExchanged; // 
+        numBottles = numBottles % numExchange; // see how many bottles left
+        numBottles += bottleExchanged; // add bottles we get to the bottles we have
+    }
+    return total;
+  }
+}
