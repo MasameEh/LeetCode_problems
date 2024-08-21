ad9e55fb3010afcbb8beb7bab3e79664a18d6c7f
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2024-08-21 14:35:47 +0300

    Time: 344 ms (75.81%), Space: 147 MB (91.94%) - LeetHub

---
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.dart
@@ -0,0 +1,13 @@
+class Solution {
+  int findPermutationDifference(String s, String t) {
+    int sum = 0;
+    for(int i = 0; i < s.length; i++){
+        for(int j = 0; j < t.length; j++){
+            if(s[i] == t[j]){
+                sum += (i - j).abs();
+            }
+        }
+    }
+    return sum;
+  }
+}
