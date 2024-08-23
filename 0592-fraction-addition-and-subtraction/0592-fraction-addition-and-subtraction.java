5e78e10c621f00a8b62a450dfcf298da3597f043
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2024-08-23 19:59:15 +0300

    Time: 4 ms (84.86%), Space: 41.6 MB (85.78%) - LeetHub

---
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+    public String fractionAddition(String expression) {
+        int numerator = 0, denominator = 1;
+        int i = 0, n = expression.length();
+
+        while (i < n) {
+            int sign = 1;
+            // Extract the sign 
+            if (expression.charAt(i) == '+' || expression.charAt(i) == '-') {
+                sign = expression.charAt(i) == '-' ? -1 : 1;
+                i++;
+            }
+
+            int num = 0;
+            while (i < n && Character.isDigit(expression.charAt(i))) {
+                num = num * 10 + (expression.charAt(i) - '0');
+                i++;
+            }
+
+            num *= sign;
+            i++; // Skip the '/'
+
+            int denom = 0;
+
+            while (i < n && Character.isDigit(expression.charAt(i))) {
+                denom = denom * 10 + (expression.charAt(i) - '0');
+                i++;
+            }
+            numerator = numerator * denom + num * denominator;
+            denominator *= denom;
+
+            int gcd = gcd(Math.abs(numerator), denominator);
+            numerator /= gcd;
+            denominator /= gcd;
+        }
+
+        return numerator + "/" + denominator;
+    }
+}
