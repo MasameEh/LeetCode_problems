d0f9ed0b355e1d8ce51dc934fe06371b02c7e240
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2024-09-15 19:39:54 +0300

    Time: 1243 ms (72.99%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -4,7 +4,8 @@ FROM SalesPerson
 WHERE name NOT IN (SELECT s.name
     FROM SalesPerson s
     JOIN Orders o ON s.sales_id = o.sales_id
-    JOIN Company c ON o.com_id = c.com_id AND c.name = 'RED')
+    JOIN Company c ON o.com_id = c.com_id 
+    WHERE c.name = 'RED')
 
 
 
