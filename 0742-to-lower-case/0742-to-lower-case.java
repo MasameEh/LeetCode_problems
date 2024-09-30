a05091021d15ecef48fa1236ef19a9b78c81832b
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2024-09-30 13:35:44 +0300

    Time: 6 ms (7.39%), Space: 42.1 MB (7.81%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,16 +1,13 @@
 class Solution {
     public String toLowerCase(String s) {
-        StringBuilder ret = new StringBuilder(); // Use StringBuilder for better string manipulation
-        
+        String res = "";
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            if(c >= 'A' && c <= 'Z'){ // Check if character is uppercase
-                ret.append((char)(c + 32)); // Convert to lowercase by adding 32
-            } else {
-                ret.append(c); // If it's already lowercase, just add it
+            if(c <= 90 && c >= 65){
+                c+= 32;
             }
+            res+=c;
         }
-        
-        return ret.toString(); // Convert StringBuilder to String and return
+        return res;  
     }
 }
