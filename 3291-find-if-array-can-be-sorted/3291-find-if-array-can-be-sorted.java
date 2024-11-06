4b8602fa71dbe155a107688c633e668915e62e28
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2024-11-06 19:13:24 +0200

    Time: 2 ms (60%), Space: 44.3 MB (61.64%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
@@ -1,37 +1,42 @@
 class Solution {
-    private int countSetBits(int n){
+    private int countSetBits(int n) {
         int count = 0;
-        while(n > 0){
-            n&=(n-1);
+        while (n > 0) {
+            n &= (n - 1);
             count++;
         }
         return count;
     }
-    public boolean canSortArray(int[] nums) {
-        boolean isPossible = true;
 
-        for(int i = 0; i < nums.length - 1; i++){
+    public boolean canSortArray(int[] nums) {
+        // Step 1: Count set bits for each number and store in a map
+        int[] bitCounts = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            bitCounts[i] = countSetBits(nums[i]);
+        }
+
+        // Step 2: Use Bubble Sort only when set bits are equal
+        for (int i = 0; i < nums.length - 1; i++) {
             boolean isSwapped = false;
 
-            for(int j = 0; j < nums.length - i - 1; j++){
-                if(nums[j] > nums[j+1]){
-                    int num1Bits = countSetBits(nums[j]);
-                    int num2Bits = countSetBits(nums[j+1]);
-                    System.out.println(num1Bits + " " + num2Bits);
-                    if(num1Bits == num2Bits){
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    if (bitCounts[j] == bitCounts[j + 1]) {
+                        // Swap only if the number of set bits is the same
                         int temp = nums[j];
-                        nums[j] = nums[j+1];
-                        nums[j+1] = temp;
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
                         isSwapped = true;
-                    }else{
-                        isPossible = false;
-                        return isPossible;
+                    } else {
+                        return false;  // Not possible to sort
                     }
                 }
             }
-            if(!isSwapped) return true;
+
+            // If no swaps occurred, the array is sorted
+            if (!isSwapped) return true;
         }
-        return isPossible;
-        
+
+        return true;
     }
 }
