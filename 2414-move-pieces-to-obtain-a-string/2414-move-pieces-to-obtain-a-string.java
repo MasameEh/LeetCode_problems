ec66ef195ecc4ff856f1b53bcf419518bd7b9be1
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2024-12-05 22:19:30 +0200

    Time: 41 ms (32.96%), Space: 46.2 MB (15.08%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
@@ -1,30 +1,30 @@
 class Solution {
     public boolean canChange(String start, String target) {
-        int n = start.length();
-        int i = 0, j = 0;
+        if (!start.replace("_", "").equals(target.replace("_", ""))) {
+            return false;
+        }
+        
+        for(int i = 0, j = 0; i < start.length(); i++){
+            char c = start.charAt(i);
 
-        while (i < n || j < n) {
-            while (i < n && start.charAt(i) == '_') i++;
-            while (j < n && target.charAt(j) == '_') j++;
-
-            if (i == n && j == n) return true;
-
-            if (i == n || j == n) return false;
-
-            char c1 = start.charAt(i);
-            char c2 = target.charAt(j);
-
-            if(c1 != c2){
-                return false;
+            if(c == '_'){
+                continue;
             }else{
-                if(c1 == 'L' && i < j) return false;
-                if(c1 == 'R' && i > j) return false;
+                while (target.charAt(j) == '_') {
+                    j++;
+                }
+                if(c == 'L'){
+                    if (i < j) {
+                        return false;
+                    }
+                }else{
+                    if (i > j) {
+                        return false;
+                    }
+                }
             }
-            
-            i++;
             j++;
         }
-
         return true;
     }
 }
