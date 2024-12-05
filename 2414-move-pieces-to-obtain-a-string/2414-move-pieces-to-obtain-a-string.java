c0daf045080599f788ed03e9795fd3d98f4079e1
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2024-12-05 22:13:03 +0200

    Time: 13 ms (92.18%), Space: 46 MB (17.59%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
@@ -4,27 +4,23 @@ class Solution {
         int i = 0, j = 0;
 
         while (i < n || j < n) {
-            // Skip '_' in both strings
             while (i < n && start.charAt(i) == '_') i++;
             while (j < n && target.charAt(j) == '_') j++;
 
-            // If both pointers reach the end, strings are valid
             if (i == n && j == n) return true;
 
-            // If only one pointer reaches the end, mismatch
             if (i == n || j == n) return false;
 
-            // Check character match
             char c1 = start.charAt(i);
             char c2 = target.charAt(j);
 
-            if (c1 != c2) return false;
-
-            // Check movement constraints
-            if (c1 == 'L' && i < j) return false; // L cannot move right
-            if (c1 == 'R' && i > j) return false; // R cannot move left
-
-            // Move both pointers
+            if(c1 != c2){
+                return false;
+            }else{
+                if(c1 == 'L' && i < j) return false;
+                if(c1 == 'R' && i > j) return false;
+            }
+            
             i++;
             j++;
         }
