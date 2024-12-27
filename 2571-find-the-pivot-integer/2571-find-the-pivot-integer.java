7c6db8b59a9d2d0ac0cd775f09d64110053d07b5
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2024-12-28 00:18:26 +0200

    Time: 1 ms (63.76%), Space: 41.3 MB (8.62%) - LeetHub

---
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pivotInteger(int n) {
+
+        int[] prefix = new int[n+1];
+        prefix[0] = 0;
+
+        for(int i = 1; i <= n; i++){
+            prefix[i] = prefix[i-1] + i;
+        }
+
+        for(int i = 1; i <= n; i++){
+            if(prefix[i] == prefix[n] - prefix[i-1]){
+                return i;
+            }
+        }
+        return -1; // no pivot
+    }
+}
