3cbc547204b8a92e205171e4ee2625b7c96d7b60
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2025-02-01 00:55:28 +0200

    Time: 5 ms (72.44%), Space: 44.4 MB (18.29%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -2,16 +2,16 @@ class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0) return false;
         if(x < 9) return true;
+        
+        int newX = 0;
+        int num = x;
 
-        String xStr = String.valueOf(x);
-        String revStr = "";
-
-        for(int i = 0; i < xStr.length(); i++){
-            char ch = xStr.charAt(i);
-
-            revStr = ch + revStr;
+        while(num > 0){
+            int rem = num % 10;
+            newX = newX * 10 + rem;
+            num/=10;
         }
 
-        return xStr.equals(revStr);
+        return newX == x;
     }
 }
