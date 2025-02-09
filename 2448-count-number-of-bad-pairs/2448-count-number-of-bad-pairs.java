96d61caf46b5d65f9747505c4511ae66604af147
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2025-02-10 01:55:40 +0200

    Time: 36 ms (72.94%), Space: 60.9 MB (10.29%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
@@ -1,16 +1,16 @@
-
 class Solution {
     public long countBadPairs(int[] nums) {
         int n = nums.length;
-        long totalPairs = (long)n * (n - 1) / 2;
-        long goodPairs = 0L;
+        Map<Integer, Long> trackMap = new HashMap<>();
+        long totalPairs = (long)n * (n-1) / 2;
+        long  goodPairs = 0L;
 
-        Map<Integer, Long> diffCount = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            int diff = nums[i] - i;
-            goodPairs += diffCount.getOrDefault(diff, 0L);
-            diffCount.put(diff, diffCount.getOrDefault(diff, 0L) + 1);
+        for(int i = 0; i < n; i++){
+            long freq = trackMap.getOrDefault(nums[i] - i, 0L);
+            goodPairs+=freq;
+            trackMap.put(nums[i] - i, (long)freq + 1);
         }
-        return totalPairs - goodPairs;
+
+       return totalPairs - goodPairs;
     }
 }
