2d36a68e26acff602ee91ae4dcb15acc67375694
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2025-03-21 04:36:45 +0200

    Time: 20 ms (20.99%), Space: 47.3 MB (15.19%) - LeetHub

--- 3778-transform-array-by-parity/3778-transform-array-by-parity.kt
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.kt
@@ -1,9 +1,5 @@
 class Solution {
     fun transformArray(nums: IntArray): IntArray {
-
-        for(i in nums.indices){
-            if(nums[i] % 2 == 0) nums[i] = 0 else nums[i] = 1
-        }
-        return nums.sortedArray()
+        return nums.map { if (it % 2 == 0) 0 else 1 }.sorted().toIntArray()
     }
 }
