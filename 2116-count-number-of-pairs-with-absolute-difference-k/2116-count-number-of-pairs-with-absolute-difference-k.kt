15d9dc0e81e539affb602fab690cb785e3ce0a11
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2025-03-27 01:03:08 +0200

    Time: 7 ms (80.77%), Space: 45.9 MB (11.54%) - LeetHub

---
+++ 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun countKDifference(nums: IntArray, k: Int): Int {
+        var count = 0 
+        for(i in nums.indices){
+            for(j in i+1 until nums.size){
+                if(abs(nums[i] - nums[j]) == k){
+                    count++
+                }
+            }
+        }
+        return count
+    }
+}
