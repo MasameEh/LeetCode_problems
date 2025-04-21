d045cc3ba98bf67527f93b5f504ec9ad30b6db68
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2025-04-21 23:40:40 +0200

    Time: 30 ms (5.1%), Space: 48.5 MB (52.36%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.kt
@@ -0,0 +1,34 @@
+class Solution {
+    fun canPlaceFlowers(flowerbed: IntArray, n: Int): Boolean {
+        var count = n
+        if(n == 0) return true
+        if (flowerbed.size == 1) {
+            return if (flowerbed[0] == 0) count - 1 <= 0 else count <= 0
+        }
+        for(i in flowerbed.indices){
+            if(flowerbed[i] == 0){
+                if(i != 0 && i != flowerbed.size - 1){
+                    if(flowerbed[i + 1] != 1 && flowerbed[i - 1] != 1){
+                        count--
+                        flowerbed[i] = 1
+                        println(i)
+                    }
+                }
+                if(i == 0 && flowerbed[i + 1] != 1){
+                    count--
+                    flowerbed[i] = 1
+                    println(i)
+                } else if(i == flowerbed.size - 1 && flowerbed[i - 1] != 1){
+                    count--
+                    flowerbed[i] = 1
+                    println(i)
+                }
+            }
+            if(count == 0){
+                return true
+            }
+        }
+
+        return false
+    }
+}
