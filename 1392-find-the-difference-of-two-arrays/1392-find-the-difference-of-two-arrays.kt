fe9e94773b08cb4dce27c072d6b3a7028031f414
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2025-04-28 00:12:50 +0300

    Time: 25 ms (93.2%), Space: 51.7 MB (68.64%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.kt
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.kt
@@ -1,20 +1,13 @@
 class Solution {
     fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
-        val uniqueNum1 = mutableSetOf<Int>()
-        val uniqueNum2 = mutableSetOf<Int>()
 
         val diffNums = mutableListOf<MutableList<Int>>()
-
         diffNums.add(mutableListOf())
         diffNums.add(mutableListOf())
 
-        for(i in nums1.indices){
-            uniqueNum1.add(nums1[i])
-        }
-        for(i in nums2.indices){
-            uniqueNum2.add(nums2[i])
-        }
-        
+        val uniqueNum1 = nums1.toSet()
+        val uniqueNum2 = nums2.toSet()
+
         for(num in uniqueNum1){
 
             if(!uniqueNum2.contains(num)){
