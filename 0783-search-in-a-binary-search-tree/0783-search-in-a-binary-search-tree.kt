986ed0b8ab6602947d227702334f8c13676a793c
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2025-04-29 00:50:57 +0300

    Time: 0 ms (100%), Space: 48.8 MB (68.63%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.kt
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.kt
@@ -11,33 +11,21 @@
 class Solution {
     private fun inOrder(root: TreeNode?, `val`: Int): TreeNode?{
         var node = root
-                
-        if(`val` == node?.`val`){
-        
+        if(root == null) return null
+
+        if (`val` == node?.`val`) {
             return node
         }
-   
-    val leftResult = inOrder(node?.left, `val`)
-    if (leftResult != null) {
-        return leftResult
-    }
 
-    // Search in the right subtree
-    val rightResult = inOrder(node?.right, `val`)
-    if (rightResult != null) {
-        return rightResult
-    }
+        val leftResult = inOrder(root.left, `val`)
+        if (leftResult != null) {
+            return leftResult
+        }
 
-        return null
+        return inOrder(root.right, `val`)
     }
     fun searchBST(root: TreeNode?, `val`: Int): TreeNode? {
-        var node = root
-        if(root == null) return null
-        if(node.`val` == `val`) return node
 
-        if(`val` > node.`val`){
-            return searchBST(node.right, `val`)
-        }else return searchBST(node.left,`val`)
-        return null
+        return inOrder(root, `val`)
     }
 }
