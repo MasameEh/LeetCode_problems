91b8632f908e88c8e44593aeb6321823de559b12
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2025-06-04 02:58:15 +0300

    Time: 15 ms (37.88%), Space: 45.7 MB (36.36%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun calPoints(operations: Array<String>): Int {
+
+        val ansList = mutableListOf<Int>()
+
+        for(i in 0 until operations.size ){
+            if(operations[i].toIntOrNull() != null){
+                ansList.add(operations[i].toIntOrNull()!!)
+            }else{
+                when(operations[i]){
+                    "D" ->{ 
+                            val doubled = ansList.last() * 2
+                            ansList.add(doubled) 
+                        }
+                    "C" ->{
+                        ansList.removeLast()
+                    } 
+                    "+" ->{
+                        val secondToLast = ansList.getOrNull(ansList.size - 2) ?: 0
+                        ansList.add(secondToLast + ansList.last())
+                    } 
+                    else -> println("")
+                }   
+            }
+        }
+        return ansList.sum()
+    }
+}
