d67a114a7668d199b1aa3e17a782922e3100d529
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2025-06-14 18:13:22 +0300

    Time: 2 ms (26.39%), Space: 42.9 MB (5.56%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.kt
+++ 0742-to-lower-case/0742-to-lower-case.kt
@@ -2,7 +2,7 @@ class Solution {
     fun toLowerCase(s: String): String {
         var ans = ""
         for(c in s){
-            if(c >= 'A' && c <= 'Z'){
+            if(c in 'A'..'Z'){
                 ans += c + 32
             }else ans += c
             
